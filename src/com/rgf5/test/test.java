60c8b0f4dafc7c85624de9aa87f44712e74ad0ec
商听寒 <316374157@qq.com>
2020-05-20 18:10:12 +0800

    phj version 0.3

--- src/com/rgf5/test/test.java
+++ src/com/rgf5/test/test.java
@@ -1,10 +1,10 @@
 package com.rgf5.test;
-
 import com.rgf5.bean.Admin;
 import com.rgf5.dao.BaseDao;
 import com.rgf5.utils.JDBCUtils;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.util.List;
 
@@ -32,9 +32,9 @@ public class test {
      */
     @Test
     public void test1(){
-        //String sql = "select id,admin_id as adminId,passWd from admins";
-        String sql = "select id,admin_id as adminId,passWd from admins where id=? and passWd = ?";
-        Admin admin = BaseDao.getBean(sql, Admin.class,1,"1");
+        String sql = "select id,admin_id as adminId,passWd from admins";
+        //String sql = "select id,admin_id as adminId,passWd from admins where id=? and passWd = ?";
+        Admin admin = BaseDao.getBean(sql, Admin.class);
         System.out.println(admin);
     }
 
@@ -57,7 +57,7 @@ public class test {
     @Test
     public void test3(){
         String sql = "update admins set passWd = ? where id=?";
-        boolean update = BaseDao.update(sql,"666",2);
+        boolean update = BaseDao.update(sql,"666",3);
         sql = "select * from admins";
         List<Admin> beanList = BaseDao.getBeanList(sql, Admin.class);
         System.out.println(update);
@@ -70,10 +70,18 @@ public class test {
     @Test
     public void test4(){
         String sql = "delete from admins where id=?";
-        boolean update = BaseDao.update(sql,2);
+        boolean update = BaseDao.update(sql,3);
         sql = "select * from admins";
         List<Admin> beanList = BaseDao.getBeanList(sql, Admin.class);
         System.out.println(update);
         System.out.println(beanList);
+    }
+
+    @Test
+    public void test5(){
+        Method[] declaredMethods = Admin.class.getDeclaredMethods();
+        for (Method method : declaredMethods) {
+            System.out.println(method.getName());
+        }
     }
 }
--- src/com/rgf5/utils/WebUtils.java
+++ src/com/rgf5/utils/WebUtils.java
@@ -23,7 +23,6 @@ public class WebUtils {
         }catch (Exception e){
             e.printStackTrace();
         }
-
         return bean;
     }
 }
