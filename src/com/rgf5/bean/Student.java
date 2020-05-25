6f92f90570d7d8ec64adeef41601dd6e6e35f65b
商听寒 <316374157@qq.com>
2020-05-25 15:03:28 +0800

    phj version 0.5

--- src/com/rgf5/bean/Student.java
+++ src/com/rgf5/bean/Student.java
@@ -94,6 +94,7 @@ public class Student {
                 ", passWd='" + passWd + '\'' +
                 ", sex='" + sex + '\'' +
                 ", classId='" + classId + '\'' +
+                ", studentName='" + studentName + '\'' +
                 '}';
     }
 
--- src/com/rgf5/bean/Teacher.java
+++ src/com/rgf5/bean/Teacher.java
@@ -122,6 +122,7 @@ public class Teacher {
                 ", courseId1='" + courseId1 + '\'' +
                 ", courseId2='" + courseId2 + '\'' +
                 ", courseId3='" + courseId3 + '\'' +
+                ", teacherName='" + teacherName + '\'' +
                 '}';
     }
 
