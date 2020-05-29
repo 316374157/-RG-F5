2d3bf64c15392e1335beb5dc273d908ed9f852d1
商听寒 <316374157@qq.com>
2020-05-29 18:13:32 +0800

    phj version 0.8.4

--- src/com/rgf5/controller/FileServlet.java
+++ src/com/rgf5/controller/FileServlet.java
@@ -1,5 +1,9 @@
 package com.rgf5.controller;
 
+import com.rgf5.bean.DataBank;
+import com.rgf5.bean.Teacher;
+import com.rgf5.service.DataBankService;
+import com.rgf5.service.impl.DataBankServiceImpl;
 import com.rgf5.utils.WebUtils;
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "FileServlet",value = "/FileServlet")
 public class FileServlet extends HttpServlet {
@@ -15,7 +20,14 @@ public class FileServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
-        WebUtils.fileUpLoad(request);
+        DataBank dataBank = WebUtils.fileUpLoad(request);
+        DataBankService dataBankService = new DataBankServiceImpl();
+        HttpSession session = request.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("teacher");
+        dataBank.setAuthorId(teacher.getId());
+        dataBank.setAuthorName(teacher.getTeacherName());
+        dataBankService.add(dataBank);
+        request.getRequestDispatcher("DataBankServlet?method=getFileByCourseAndClass").forward(request, response);
     }
 
     @Override
--- src/com/rgf5/utils/WebUtils.java
+++ src/com/rgf5/utils/WebUtils.java
@@ -2,19 +2,24 @@ package com.rgf5.utils;
 
 import com.rgf5.bean.Classes;
 import com.rgf5.bean.Course;
+import com.rgf5.bean.DataBank;
 import com.rgf5.service.ClassService;
 import com.rgf5.service.CourseService;
 import com.rgf5.service.impl.ClassServiceImpl;
 import com.rgf5.service.impl.CourseServiceImpl;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.IOUtils;
 
 ;
 
@@ -37,9 +42,10 @@ public class WebUtils {
         return bean;
     }
 
-    public static void fileUpLoad(HttpServletRequest request){
+    public static DataBank fileUpLoad(HttpServletRequest request){
         ServletContext servletPath = request.getServletContext();
         String rootPath = servletPath.getRealPath("databank");
+        DataBank dataBank = new DataBank();
         Classes classes = new Classes();
         Course course = new Course();
         CourseService courseService = new CourseServiceImpl();
@@ -47,35 +53,40 @@ public class WebUtils {
         LinkedHashMap<String, String> map = new LinkedHashMap<>();
         DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
         ServletFileUpload fileUpload = new ServletFileUpload(fileItemFactory);
+        fileUpload.setHeaderEncoding("utf-8");
         try {
             List<FileItem> fileItems = fileUpload.parseRequest(request);
             for (FileItem fileItem : fileItems) {
                 if(fileItem.isFormField()){
                     map.put(fileItem.getFieldName(), fileItem.getString("utf-8"));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        classes.setClassName(map.get("className"));
-        course.setCourseName(map.get("courseName"));
-        course = courseService.getBeanByCourseName(course);
-        classes = classService.getBeanByClassName(classes);
-        System.out.println(classes);
-        System.out.println(course);
-        rootPath = rootPath+"\\"+classes.getClassId()+"\\"+course.getCourseId()+"\\";
-        System.out.println(rootPath);
-        try {
-            List<FileItem> fileItems = fileUpload.parseRequest(request);
-            for (FileItem fileItem : fileItems) {
-                if(fileItem.isFormField()){
-                    System.out.println("普通");
                 }else {
-                    System.out.println("文件");
+                    classes.setClassName(map.get("className"));
+                    course.setCourseName(map.get("courseName"));
+                    course = courseService.getBeanByCourseName(course);
+                    classes = classService.getBeanByClassName(classes);
+                    System.out.println(classes);
+                    System.out.println(course);
+                    rootPath = rootPath+"\\"+classes.getClassId()+"\\"+course.getCourseId()+"\\";
+                    System.out.println(rootPath);
+                    String name = fileItem.getName();
+                    dataBank.setDataName(name);
+                    dataBank.setDataName(rootPath);
+                    dataBank.setClassId(classes.getClassId());
+                    dataBank.setClassId(course.getCourseId());
+                    dataBank.setDataType(map.get("dataType"));
+                    System.out.println(name);
+                    InputStream inputStream = fileItem.getInputStream();
+                    name = UUID.randomUUID().toString().replace("-", "")
+                            .substring(0, 10)+"_"+name;
+                    System.out.println(name);
+                    FileOutputStream outputStream = new FileOutputStream(rootPath+name);
+                    IOUtils.copy(inputStream,outputStream);
+                    outputStream.close();
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return dataBank;
     }
 }
