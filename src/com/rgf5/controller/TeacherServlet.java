a39170bb1143fc5671f0c87e29f9beb8e0b02e72
zffzffzff <1132271952@qq.com>
2020-05-30 12:49:39 +0800

    zff 1.2

--- src/com/rgf5/controller/TeacherServlet.java
+++ src/com/rgf5/controller/TeacherServlet.java
@@ -1,21 +1,27 @@
 package com.rgf5.controller;
 
 import com.rgf5.bean.Classes;
+import com.rgf5.bean.Student;
 import com.rgf5.bean.Teacher;
+import com.rgf5.dao.ClassDao;
+import com.rgf5.dao.StudentDao;
+import com.rgf5.dao.impl.ClassDaoImpl;
+import com.rgf5.dao.impl.StudentDaoImpl;
 import com.rgf5.service.ClassService;
 import com.rgf5.service.CourseService;
 import com.rgf5.service.TeacherService;
 import com.rgf5.service.impl.ClassServiceImpl;
 import com.rgf5.service.impl.CourseServiceImpl;
 import com.rgf5.service.impl.TeacherServiceImpl;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "TeacherServlet",value = "/TeacherServlet")
 public class TeacherServlet extends BaseServlet {
@@ -41,17 +47,24 @@ public class TeacherServlet extends BaseServlet {
     }
 
     protected void getMyAllClass(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        TeacherService teacherService = new TeacherServiceImpl();
         HttpSession session = request.getSession();
         Teacher teacher = (Teacher) session.getAttribute("teacher");
         ClassService classService = new ClassServiceImpl();
         List<Classes> myBeanList = classService.getMyBeanList(teacher);
         request.setAttribute("myBeanList",myBeanList);
         request.getRequestDispatcher("pages/teacher/MyAllClass.jsp").forward(request, response);
-        System.out.println("teacherService="+teacherService);
-        System.out.println("session="+session);
-        System.out.println("teacher="+teacher);
-        System.out.println("classService="+classService);
-        System.out.println("myBeanList="+myBeanList);
+    }
+
+    protected void getClass(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        StudentDao studentDao = new StudentDaoImpl();
+        ClassDao classDao = new ClassDaoImpl();
+        String classId = request.getParameter("classId");
+        Classes beanByClassId = classDao.getBeanByClassId(classId);
+        List<Student> beanListByClassId = studentDao.getBeanListByClassId(classId);
+        request.setAttribute("beanByClassId",beanByClassId);
+        System.out.println(beanByClassId);
+        request.setAttribute("beanListByClassId",beanListByClassId);
+        request.getRequestDispatcher("pages/teacher/Class.jsp").forward(request, response);
     }
 }
